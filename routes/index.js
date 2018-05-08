f13b5c34bc8d3814d94dff496d7d032156dd4931
Bala Nyan Kyaw <balanyan.kyaw@sjsu.edu>
2018-05-07 21:22:35 -0700

    Jenkins

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-	console.log("hey");
+  console.log('Hello Elly')
   res.render('index', {
     title: 'Simple Node Authentication',
     userinfo: req.userinfo,

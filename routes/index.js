7c0be96860c474b322967bb43de276cfc4b57ccf
Bala Nyan Kyaw <balanyan.kyaw@sjsu.edu>
2018-05-07 23:15:23 -0700

    hi

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  console.log('Hello Elly')
+	console.log("hey");
   res.render('index', {
     title: 'Simple Node Authentication',
     userinfo: req.userinfo,
